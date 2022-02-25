d4601f246795ca4b5a494a56bc4b6b6847cb0351
Cattani <gcattani1@gmail.com>
2022-02-24 22:41:27 -0300

    Separate faulty enum values

--- db/migrate/20220223205128_add_enum_values_of_property_sell.rb
+++ db/migrate/20220223205128_add_enum_values_of_property_sell.rb
@@ -4,9 +4,15 @@ class AddEnumValuesOfPropertySell < ActiveRecord::Migration[6.1]
   disable_ddl_transaction!
 
   def up
-    add_enum_value 'touchpoint_type',       %i[realtor_feedback homeday_service_feedback], after: 'pending'
-    add_enum_value 'respondent_class_type', %i[Seller Buyer], after: 'Pending'
-    add_enum_value 'object_class_type',     %i[Realtor HomedayService], after: 'Pending'
+    # Can't use %i[...] here because of no support in PGEnum
+    add_enum_value 'touchpoint_type',       :realtor_feedback,         after: 'pending'
+    add_enum_value 'touchpoint_type',       :homeday_service_feedback, after: 'pending'
+
+    add_enum_value 'respondent_class_type', :Seller, after: 'Pending'
+    add_enum_value 'respondent_class_type', :Buyer,  after: 'Pending'
+
+    add_enum_value 'object_class_type',     :Realtor,        after: 'Pending'
+    add_enum_value 'object_class_type',     :HomedayService, after: 'Pending'
   end
 
   def down
