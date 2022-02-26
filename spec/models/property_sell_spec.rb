b987a42caf65384a9394d15b2a35447cbbaa05a8
Cattani <gcattani1@gmail.com>
2022-02-26 09:00:20 -0300

    Add inclusion scopes tests

--- spec/models/property_sell_spec.rb
+++ spec/models/property_sell_spec.rb
@@ -3,7 +3,28 @@
 require 'rails_helper'
 
 RSpec.describe PropertySell, type: :model do
-  subject { FactoryBot.build(:property_sell) }
+  subject { FactoryBot.build(:property_sell, scorable_class: 'Realtor', touchpoint: 'realtor_feedback') }
 
   it_should_behave_like 'a net promoter score'
+
+  context 'validation of inclusion scopes' do
+    context 'scorable class is Realtor' do
+      it 'allows for realtor related touchpoints' do
+        expect(subject).to be_valid
+
+        subject.scorable_class = 'HomedayService'
+        expect(subject).not_to be_valid
+      end
+    end
+    context 'scorable class is HomedayService' do
+      it 'allows for Homeday related touchpoints' do
+        subject.scorable_class = 'HomedayService'
+        subject.touchpoint = 'homeday_service_feedback'
+        expect(subject).to be_valid
+
+        subject.scorable_class = 'Realtor'
+        expect(subject).not_to be_valid
+      end
+    end
+  end
 end
