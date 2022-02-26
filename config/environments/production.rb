704a4eccece5d1b9584bf0d765b6a83fbe1c0fbb
Cattani <gcattani1@gmail.com>
2022-02-25 23:15:32 -0300

    Force SSL in prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,9 @@
 Rails.application.configure do
   config.cache_classes = true
   config.eager_load = true
+
+  # We need this to make sure that basic auth won't be sent as plain text
+  config.force_ssl = true
   config.consider_all_requests_local = false
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
   config.active_storage.service = :local
